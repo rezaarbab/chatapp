e44f58d3aed776413e76bd354d5f413510dbede9
rezaarbab <rezaarbab@users.noreply.github.com>
2026-09-11 00:35:02 +0330

    CI: restore proven AGP 8.7.3 single-invocation emulator flow + retry + logcat

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@ plugins {
     // libsignal-client 0.102.1 ships Kotlin 2.2 metadata; compiler must be >= 2.2
     kotlin("jvm") version "2.2.20" apply false
     kotlin("android") version "2.2.20" apply false
-    id("com.android.library") version "8.13.2" apply false
+    id("com.android.library") version "8.7.3" apply false
 }
